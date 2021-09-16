bcbeff10744ce356135053ec69328a53128b2bc4
Philip M. Papadopoulos <ppapadop@uci.edu>
2021-09-16 14:04:59 -0700

    Remove files no longer required in set-based build

--- yamlspecs/IntelDefs.mk
+++
@@ -1,4 +0,0 @@
-# These are common defs used in the top-level Makefile and 
-# and Makefile.intel 
-INTELPKGS = openmpi openmpi-module mpich mpich-module
-INTELTARGETS = $(addsuffix .pkg, $(INTELPKGS))
